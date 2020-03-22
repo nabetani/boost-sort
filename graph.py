ce71297cd9e0fc1bd3b390c7a9d444e0370dcf76
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-03-22 11:38:51 +0900

    グラフ書く

--- graph.py
+++ graph.py
@@ -26,6 +26,14 @@ def write_pdf(fn):
             pdf.savefig()
 
 
+def write_pdf(fn):
+    with PdfPages(fn) as pdf:
+        fignums = pyplot.get_fignums()
+        for fignum in fignums:
+            pyplot.figure(fignum)
+            pdf.savefig()
+
+
 def uniq_items(a, ix):
     return list(set(x[ix] for x in a))
 
@@ -33,6 +41,15 @@ def uniq_items(a, ix):
 def get_ticks(data, compiler, src, sorter, sizes):
     r = []
     for size in sizes:
+        for line in data:
+            if line[0] == compiler and line[1] == sorter and line[2] == src and line[3] == size:
+                r.append(float(line[5]))
+    return r
+
+
+def get_ticks2(data, compiler, src, sorters, size):
+    r = []
+    for sorter in sorters:
         for line in data:
             if line[0] == compiler and line[1] == sorter and line[2] == src and line[3] == size:
                 r.append(float(line[5]))
@@ -64,20 +81,56 @@ def add_page(fig, compiler, data):
     fig.align_labels()  # 同一ページ内の y-label の位置などを揃える
 
 
+def barchart(fig, size, data):
+    fig.suptitle("Time required to sort %d elements (ms)" % size)
+    compilers = uniq_items(data, 0)
+    sorters = list(reversed([
+        "std::sort",
+        "std::stable_sort",
+        "boost/flat_stable_sort",
+        "boost/pdq_sort",
+        "boost/spin",
+        "boost/spread",
+    ]))
+    srces = [
+        "random",
+        "sorted",
+        "almost_sorted",
+        "reverse",
+        "few_types",
+    ]
+    for ix in range(len(srces)):
+        src = srces[ix]
+        for cix in range(len(compilers)):
+            compiler = compilers[cix]
+            graph = fig.add_subplot(
+                len(srces),
+                len(compilers), ix*len(compilers)+cix+1)
+            graph.set_title(compiler + " / " + src)
+            ticks = get_ticks2(data, compiler, src, sorters, str(int(size)))
+            left = list(range(len(sorters)))
+            graph.barh(left, ticks,  tick_label=sorters, align="center")
+    pyplot.tight_layout()  # グラフが別のグラフと重なるのを抑制する
+    fig.subplots_adjust(top=0.9)
+    fig.align_labels()  # 同一ページ内の y-label の位置などを揃える
+
+
 def main():
-    all_data = read_csv("list.csv")
-    compilers = uniq_items(all_data, 0)
-    sorters = uniq_items(all_data, 1)
-    srces = uniq_items(all_data, 2)
-    sizes = uniq_items(all_data, 3)
+    data = read_csv("list.csv")
+    compilers = uniq_items(data, 0)
+    sorters = uniq_items(data, 1)
+    srces = uniq_items(data, 2)
+    sizes = uniq_items(data, 3)
     print("compilers", " ".join(compilers))
     print("sorters", " ".join(sorters))
     print("srces", " ".join(srces))
     print("sizes", " ".join(sizes))
-    for compiler in compilers:
-        fig = pyplot.figure(figsize=A4_PORTRAIT)
-        add_page(fig, compiler, all_data)
+    # for compiler in compilers:
+    #     fig = pyplot.figure(figsize=A4_PORTRAIT)
+    #     add_page(fig, compiler, data)
+    barchart(pyplot.figure(figsize=A4_PORTRAIT), 1e6, data)
     write_pdf("graph.pdf")
+    pyplot.savefig('graph.png')
 
 
 main()
